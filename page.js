0d7fb87d2e3dc58ec2b4e12a827e5bb855d2f6e6
Lawrence Zhou <lawrenceh2005@gmail.com>
2022-08-12 00:36:02 -0700

    feat: cock

---
+++ page.js
@@ -0,0 +1,9 @@
+function hide() {
+        var x = document.getElementById("dick");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } 
+        else {
+            x.style.display = "none";
+        }
+}
